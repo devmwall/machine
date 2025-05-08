7ee68fd2898cfb393545740fa413368349dc5664
devmwall <me@mattwall.dev>
2025-05-08 03:22:09 +0000

    Adding info

--- setup-old.sh
+++ setup-old.sh
@@ -20,18 +20,26 @@ if [ ! -d "$DEV_ENV" ]; then
 fi
 
 # Update system
+dnf update -y
+
 # Install core packages
-#!/usr/bin/env bash
-
-sudo pacman -S --noconfirm --needed gimp tldr go fzf rofi-wayland
-sudo pacman -S love --noconfirm --needed
-sudo pacman -S ttf-font-awesome waybar ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
-sudo pacman -S hyprlock --noconfirm --needed
-sudo pacman -S ghostty
-sudo pacman -S hyprpaper
-paru -Syu wlogout-git --noconfirm --needed
-sudo pacman -S code
-
+dnf install -y \
+    i3 \
+    neovim \
+    tmux \
+    gimp \
+    python3 \
+    python3-pip \
+    NetworkManager-tui \
+    rofi \
+    xrandr \
+    nm-applet \
+    brightnessctl \
+    pgadmin4 \
+    ripgrep \
+    lua \
+    luarocks \
+    go
 
 flatpak install flathub md.obsidian.Obsidian
 flatpak install org.pgadmin.pgadmin4
@@ -44,6 +52,10 @@ cd /tmp/luarocks-3.11.0
 ln -s /var/lib/flatpak/exports/bin/md.obsidian.Obsidian /usr/bin/Obsidian
 ln -s /var/lib/flatpak/exports/bin/org.pgadmin.pgadmin4 /usr/bin/pgadmin4
 
+dnf copr enable pgdev/ghostty
+dnf install ghostty
+
+
 git config --global user.email "me@mattwall.dev"
 git config --global user.name "devmwall"
 
@@ -51,6 +63,7 @@ git config --global user.name "devmwall"
 # Visual Studio Code (VSCode) installation
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf install -y code
 
 # Create configuration directories
 mkdir -p "$ORIGINAL_HOME/.config/i3"
@@ -58,7 +71,6 @@ mkdir -p "$ORIGINAL_HOME/.config/nvim"
 mkdir -p "$ORIGINAL_HOME/.config/Code/User"
 mkdir -p "$ORIGINAL_HOME/.config/ghostty/"
 mkdir -p "$ORIGINAL_HOME/.config/keyboard/"
-mkdir -p "$ORIGINAL_HOME/.config/hypr/"
 
 
 # Copy configuration files with debug output
@@ -71,7 +83,6 @@ echo "Copying neovim config:"
 echo "Source: $DEV_ENV/env/.config/nvim/."
 echo "Destination: $ORIGINAL_HOME/.config/nvim/"
 cp -a "$DEV_ENV/env/.config/nvim/." "$ORIGINAL_HOME/.config/nvim/"
-cp -a "$DEV_ENV/env/.config/hypr/." "$ORIGINAL_HOME/.config/hypr/"
 
 echo "Copying VSCode settings:"
 echo "Source: $DEV_ENV/env/.config/vscode/settings.json"

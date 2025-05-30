9586cb560c7a94f5ce978c4abce9f8cc7ff9537c
devmwall <me@mattwall.dev>
2025-05-30 11:24:57 -0400

    Adding

--- setup.sh
+++ setup.sh
@@ -37,6 +37,7 @@ sudo pacman -S networkmanager pavucontrol
 sudo pacman -S pipewire wireplumber brightnessctl
 sudo pacman -S pipewire pipewire-pulse pipewire-alsa
 sudo pacman -S keyd
+sudo pacman -S openssh
 
 flatpak install flathub md.obsidian.Obsidian
 flatpak install org.pgadmin.pgadmin4
